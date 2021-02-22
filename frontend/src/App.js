6997b35659172131518e8859d0febe0a681e5a5b
elbytes <elnaz.alimardani@gmail.com>
2021-02-22 16:05:12 -0500

    add Header and Footer components

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,14 @@
-
+import Header from './components/Header'
+import Footer from './components/Footer'
 
 const App = () =>{
   return (
     <>
-     <h1>Welcome to The Cakery</h1>
+    <Header />
+    <main>
+      <h1>Welcome to The Cakery</h1>
+    </main>
+     <Footer />
     </>
   );
 }
