b97f77c9a2441f31e882a446bf0b42acc4517439
elbytes <elnaz.alimardani@gmail.com>
2021-02-22 17:16:40 -0500

    navbar and navbar icons

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -1,11 +1,28 @@
 import React from 'react'
-
+import {Navbar, Nav, Container} from 'react-bootstrap'
 const Header = () => {
-    return (
-        <header>
-            header
+    return <header>
+            <Navbar bg="dark" variant='dark' expand="lg" collapseOnSelect>
+            <Container>
+            <Navbar.Brand href="/">
+                <img
+                    alt=""
+                    src="../logo.png"
+                    width="30"
+                    height="30"
+                    className="d-inline-block align-top"
+                />{' '}
+            The Cakery</Navbar.Brand>
+            <Navbar.Toggle aria-controls="basic-navbar-nav" />
+            <Navbar.Collapse id="basic-navbar-nav">
+                <Nav className="ml-auto">
+                <Nav.Link href="/cart"><i class="fas fa-shopping-cart"></i> cart</Nav.Link>
+                <Nav.Link href="/login"><i class="fas fa-user"></i> sign in</Nav.Link>
+                </Nav>
+            </Navbar.Collapse>
+            </Container>
+            </Navbar>
         </header>
-    )
 }
 
 export default Header
