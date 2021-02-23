d1e068467ec171473a0da3760ce9d9034f53790d
elbytes <elnaz.alimardani@gmail.com>
2021-02-22 19:18:16 -0500

    flush out dummy product list on screen, add Product component

---
+++ frontend/src/products.js
@@ -0,0 +1,120 @@
+const products = [
+  {
+    _id: '1',
+    name: 'Strawberry Cupcake',
+    image: '/images/01.jpeg',
+    description:
+      'Strawberry Cupcake Strawberry Cupcake Strawberry Cupcake Strawberry Cupcake Strawberry Cupcake Strawberry Cupcake Strawberry Cupcake Strawberry Cupcake Strawberry Cupcake Strawberry Cupcake Strawberry Cupcake ',
+    category: 'Cupcakes',
+    price: 10.99,
+    countInStock: 10,
+    rating: 4.5,
+    numReviews: 12,
+  },
+  {
+    _id: '2',
+    name: 'Chocolate Peach Cream Cupcake',
+    image: '/images/02.jpeg',
+    description:
+      'Chocolate Peach Cream Cupcake Chocolate Peach Cream Cupcake Chocolate Peach Cream Cupcake Chocolate Peach Cream Cupcake Chocolate Peach Cream Cupcake Chocolate Peach Cream Cupcake Chocolate Peach Cream Cupcake ',
+    category: 'Cupcakes',
+    price: 10.99,
+    countInStock: 7,
+    rating: 4.0,
+    numReviews: 8,
+  },
+  {
+    _id: '3',
+    name: 'Chocolate Powder Coffee Layercake',
+    image: '/images/03.jpeg',
+    description:
+      'Chocolate Powder Coffee Layercake Chocolate Powder Coffee Layercake Chocolate Powder Coffee Layercake Chocolate Powder Coffee Layercake Chocolate Powder Coffee Layercake Chocolate Powder Coffee Layercake Chocolate Powder Coffee Layercake ',
+    category: 'Cakes',
+    price: 30.99,
+    countInStock: 5,
+    rating: 3,
+    numReviews: 12,
+  },
+  {
+    _id: '4',
+    name: 'Caramel Meringue Pie',
+    image: '/images/04.jpeg',
+    description:
+      'Caramel Meringue Pie Caramel Meringue Pie Caramel Meringue Pie Caramel Meringue Pie Caramel Meringue Pie ',
+    category: 'Pies',
+    price: 20.99,
+    countInStock: 11,
+    rating: 5,
+    numReviews: 12,
+  },
+  {
+    _id: '5',
+    name: 'Stripe Apple Pie',
+    image: '/images/05.jpeg',
+    description:
+      'Stripe Apple Pie Stripe Apple PieStripe Apple PieStripe Apple PieStripe Apple PieStripe Apple Pie',
+    category: 'Pies',
+    price: 49.99,
+    countInStock: 7,
+    rating: 3.5,
+    numReviews: 10,
+  },
+  {
+    _id: '6',
+    name: 'Blackberry Vanilla Cake',
+    image: '/images/06.jpeg',
+    description:
+      'Blackberry Vanilla Cake Blackberry Vanilla Cake Blackberry Vanilla Cake Blackberry Vanilla Cake Blackberry Vanilla Cake Blackberry Vanilla Cake ',
+    category: 'Cakes',
+    price: 29.99,
+    countInStock: 0,
+    rating: 4,
+    numReviews: 12,
+  },{
+    _id: '7',
+    name: 'Thousand Layer Chocolate Cake',
+    image: '/images/07.jpeg',
+    description:
+      'Thousand Layer Chocolate Cake Thousand Layer Chocolate Cake Thousand Layer Chocolate Cake Thousand Layer Chocolate Cake ',
+    category: 'Cakes',
+    price: 39.99,
+    countInStock: 0,
+    rating: 4,
+    numReviews: 10,
+  },{
+    _id: '8',
+    name: 'Strawberry Topped Vanilla Cupcake',
+    image: '/images/08.jpeg',
+    description:
+      'Strawberry Topped Vanilla Cupcake Strawberry Topped Vanilla Cupcake Strawberry Topped Vanilla Cupcake',
+    category: 'Cupcakes',
+    price:9.99,
+    countInStock: 0,
+    rating: 4,
+    numReviews: 10,
+  },{
+    _id: '9',
+    name: 'Macaroon Topped Strawberry Cake',
+    image: '/images/09.jpeg',
+    description:
+      'Macaroon Topped Strawberry Cake Macaroon Topped Strawberry Cake Macaroon Topped Strawberry Cake Macaroon Topped Strawberry Cake',
+    category: 'Cakes',
+    price:9.99,
+    countInStock: 2,
+    rating: 4,
+    numReviews: 12,
+  },{
+    _id: '10',
+    name: 'Chocolate Nut Cake',
+    image: '/images/010.jpeg',
+    description:
+      'Chocolate Nut Cake Chocolate Nut Cake Chocolate Nut Cake',
+    category: 'Cakes',
+    price:39.99,
+    countInStock: 2,
+    rating: 4,
+    numReviews: 9,
+  }
+]
+
+export default products
