d11f39509b68edc3f601174564d41aebd60e231b
elbytes <elnaz.alimardani@gmail.com>
2021-02-23 23:30:20 -0500

    express-async-handler middleware to deal with error checking in async routes

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,8 @@
 import express from 'express'
-import products from './data/products.js'
 import dotenv from 'dotenv'
 import connectDB from './config/database.js'
+import productRoutes from './routes/productRoutes.js'
+
 
 dotenv.config();
 
@@ -13,14 +14,7 @@ app.get('/', (req, res)=>{
     res.send('api is running...');
 })
 
-app.get('/api/products', (req, res)=>{
-    res.json(products);
-})
-
-app.get('/api/products/:id', (req, res)=>{
-    const product = products.find(p => p._id === req.params.id)
-    res.json(product);
-})
+app.use('/api/products', productRoutes);
 
 const PORT = process.env.PORT || 5001;
 app.listen(PORT, console.log(`Server running in ${process.env.NODE_ENV} on port ${PORT}`));
