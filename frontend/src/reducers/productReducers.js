9e1d2f0dd2eabf949efef1b50714a9f3c2c33194
elbytes <elnaz.alimardani@gmail.com>
2021-02-24 23:50:00 -0500

    productDetails reducer, spinner Loader Message components

--- frontend/src/reducers/productReducers.js
+++ frontend/src/reducers/productReducers.js
@@ -1,4 +1,11 @@
-import { PRODUCT_LIST_REQUEST, PRODUCT_LIST_SUCCESS, PRODUCT_LIST_FAIL} from '../constants/productConstants.js'
+import { 
+    PRODUCT_LIST_REQUEST, 
+    PRODUCT_LIST_SUCCESS, 
+    PRODUCT_LIST_FAIL, 
+    PRODUCT_DETAILS_REQUEST, 
+    PRODUCT_DETAILS_SUCCESS, 
+    PRODUCT_DETAILS_FAIL
+} from '../constants/productConstants.js'
 
 //handles state for product list on Home screen
 export const productListReducer = ( state = { products: []}, action) =>{
@@ -8,6 +15,20 @@ export const productListReducer = ( state = { products: []}, action) =>{
         case PRODUCT_LIST_SUCCESS:
             return { loading: false, products: action.payload};
         case PRODUCT_LIST_FAIL:
+            return { loading: false, error: action.payload};
+        default:
+            return state;
+    }
+}
+
+//productDetails 
+export const productDetailsReducer = ( state = { product: {reviews: []}}, action) =>{
+    switch(action.type){
+        case PRODUCT_DETAILS_REQUEST:
+            return { loading: true, ...state};
+        case PRODUCT_DETAILS_SUCCESS:
+            return { loading: false, product: action.payload};
+        case PRODUCT_DETAILS_FAIL:
             return { loading: false, error: action.payload};
         default:
             return state;
--- frontend/src/screens/Home.js
+++ frontend/src/screens/Home.js
@@ -3,6 +3,8 @@ import { Col, Row } from 'react-bootstrap'
 import Product from '../components/Product'
 import { useDispatch, useSelector } from 'react-redux'
 import {listProducts} from '../actions/productActions.js'
+import Message from '../components/Message.js'
+import Loader  from '../components/Loader.js'
 
 const Home = () => {
     const dispatch = useDispatch()
@@ -17,7 +19,7 @@ const Home = () => {
     return (
         <>
             <h1>Latest Cakery Creations</h1>
-            {loading ? <h2>Loading...</h2> : error ? <h3>{error}</h3> : <Row>
+            {loading ? <Loader /> : error ? <Message>{error}</Message> : <Row>
                     {products.map(product =>(
                         <Col key={product._id} sm={12} md={6} lg={4} xl={3}>
                             <Product product={product}/>
