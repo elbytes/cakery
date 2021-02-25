f4a823e0b5bbabedacf067939d05b6e12a5d8c13
elbytes <elnaz.alimardani@gmail.com>
2021-02-24 23:09:43 -0500

    display the fetched list on Home / useSelector

--- frontend/src/screens/Home.js
+++ frontend/src/screens/Home.js
@@ -7,22 +7,24 @@ import {listProducts} from '../actions/productActions.js'
 const Home = () => {
     const dispatch = useDispatch()
     
+    const productList = useSelector(state => state.productList);
+    const { loading, error, products } = productList;
+
     useEffect(()=>{
       dispatch(listProducts())
     }, [dispatch]);
 
-    const products = []
-
     return (
         <>
-            <h1>Latest Cakery Creations</h1>   
-            <Row>
+            <h1>Latest Cakery Creations</h1>
+            {loading ? <h2>Loading...</h2> : error ? <h3>{error}</h3> : <Row>
                     {products.map(product =>(
                         <Col key={product._id} sm={12} md={6} lg={4} xl={3}>
                             <Product product={product}/>
                         </Col>)
                     )}
-            </Row>
+            </Row>}   
+            
         </>
     )
 }
