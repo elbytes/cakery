4ce0b859c41ce393606055a3d9416834f2638925
elbytes <elnaz.alimardani@gmail.com>
2021-02-25 22:06:21 -0500

    add to cart functionality

--- frontend/src/screens/Cart.js
+++ frontend/src/screens/Cart.js
@@ -1,4 +1,25 @@
-const Cart = () => {
+import React, { useEffect } from 'react'
+import { useDispatch, useSelector} from 'react-redux'
+import { Link } from 'react-router-dom'
+import { Row, Col, ListGroup, Image, Form, Button, Card } from 'react-bootstrap'
+import Message from '../components/Message'
+import { addToCart } from '../actions/cartActions'
+
+const Cart = ({match, location, history}) => {
+    const productId = match.params.id
+    const qty = location.search ? location.search.split('=')[1] : 1 
+
+    const dispatch = useDispatch()
+    
+    const cart = useSelector(state => state.cart)
+    const { cartItems } = cart
+
+    useEffect(()=>{
+        if(productId){
+            dispatch(addToCart(productId, qty))
+        }
+    }, [dispatch, productId, qty])
+
     return (
         <div>
             Cart
--- frontend/src/screens/ProductDetails.js
+++ frontend/src/screens/ProductDetails.js
@@ -8,7 +8,7 @@ import Loader from '../components/Loader'
 import Message from '../components/Message'
 
 const ProductDetails = ({history, match}) => {
-    const [quantity, setQuantity] = useState(0);
+    const [quantity, setQuantity] = useState(1);
     
     const dispatch = useDispatch()
 
