8a06558eb6c7841f212f7791ca442c08d92ba13f
elbytes <elnaz.alimardani@gmail.com>
2021-03-01 16:29:40 -0500

    fix cart screen

--- frontend/src/screens/Cart.js
+++ frontend/src/screens/Cart.js
@@ -30,7 +30,7 @@ const Cart = ({match, location, history}) => {
                 <h1>Shopping Cart</h1>
                 {cartItems.length === 0 ? <Message>Your cart is empty<Link to='/'>  Go Back to Home</Link></Message> : (
                     <ListGroup variant='flush'>
-                        {cartItems.map(item =>{
+                        {cartItems.map(item =>(
                             <ListGroup.Item key={item.product}>
                                 <Row>
                                     <Col md={2}>
@@ -42,7 +42,7 @@ const Cart = ({match, location, history}) => {
                                     <Col md={2}>${item.price}</Col>
                                     <Col md={2}>
                                     <Form.Control as ='select' 
-                                        value={item.qty} 
+                                        value={qty} 
                                         onChange={e=>  dispatch(addToCart(item.product, Number(e.target.value)))}>
                                            { [...Array(item.countInStock).keys()].map((x) =>(
                                                 <option key={x + 1} value={x + 1}>
@@ -58,7 +58,7 @@ const Cart = ({match, location, history}) => {
                                     </Col>
                                 </Row>
                             </ListGroup.Item>
-                        })}
+                        ))}
                     </ListGroup>
                 )}
             </Col>
