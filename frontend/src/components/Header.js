5d5c088e4b305279d9f2eef94b9cd22532fe3727
elbytes <elnaz.alimardani@gmail.com>
2021-03-05 00:19:58 -0500

    change sign-in to user name when signed in

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -1,8 +1,17 @@
 import React from 'react'
-import { Navbar, Nav, Container } from 'react-bootstrap'
+import { Navbar, Nav, Container, NavDropdown } from 'react-bootstrap'
 import { LinkContainer } from 'react-router-bootstrap'
+import { useDispatch, useSelector } from 'react-redux'
 
 const Header = () => {
+    const userLogin = useSelector(state => state.userLogin)
+    const { userInfo} = userLogin
+    const dispatch = useDispatch()
+    
+    const logoutHandler = () =>{
+        console.log('logout');
+    }
+    
     return <header>
             <Navbar bg="dark" variant='dark' expand="lg" collapseOnSelect>
             <Container>
@@ -24,9 +33,17 @@ const Header = () => {
                 <LinkContainer to="/cart">
                     <Nav.Link><i className="fas fa-shopping-cart"></i> cart</Nav.Link>
                 </LinkContainer>
-                <LinkContainer to="/login">
+                {userInfo ? (
+                    <NavDropdown title={userInfo.name} id='username'>
+                        <LinkContainer to='/profile'>
+                            <NavDropdown.Item>Profile</NavDropdown.Item>
+                        </LinkContainer>
+                        <NavDropdown.Item onClick={logoutHandler}>Log Out</NavDropdown.Item>
+                    </NavDropdown>
+                ) : <LinkContainer to="/login">
                     <Nav.Link><i className="fas fa-user"></i> sign in</Nav.Link>
-                </LinkContainer>
+                </LinkContainer>}
+                
                 
                 </Nav>
             </Navbar.Collapse>
