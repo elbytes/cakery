13ad8d6c7fe39e2d712572c648c6cf123f6e7821
elbytes <elnaz.alimardani@gmail.com>
2021-03-04 21:49:02 -0500

    userRegister, middleware in userModel pre save to hash password with bcrypt

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,12 +1,11 @@
 import express from 'express'
 //import { get } from 'mongoose'
-import { authUser, getUserProfile, registerUser } from '../controllers/userController.js'
+import { authUser, registerUser, getUserProfile } from '../controllers/userController.js'
 import {protect} from '../middlewares/authenticationMiddleware.js'
 
 const router = express.Router()
 
 router.post('/login', authUser)
 router.route('/profile').get(protect, getUserProfile)
-router.post('/', registerUser)
-
+router.route('/').post(registerUser)
 export default router
